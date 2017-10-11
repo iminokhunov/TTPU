92b3c0f0549ae4764a82ca7ab39fba3cbdd46a69
abd Coldplayer <abdcoldplayer@gmail.com>
2017-10-10 18:01:20 -0700

    Welcome -> Changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Changed
                 </div>
 
                 <div class="links">
